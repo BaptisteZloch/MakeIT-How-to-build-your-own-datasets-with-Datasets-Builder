aa7a718e41aeff6822ad5d80d8654a08038e4f88
Baptiste ZLOCH <72359933+BaptisteZloch@users.noreply.github.com>
2021-03-29 10:59:10 +0200

    Software source files

---
+++ Datasets_builder/datasets_builder_form.Designer.cs
@@ -0,0 +1,372 @@
+﻿
+namespace Arduino_to_Csharp_to_excel
+{
+    partial class datasets_builder_form
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(datasets_builder_form));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.disconnect_btn = new System.Windows.Forms.Button();
+            this.baudrates = new System.Windows.Forms.ComboBox();
+            this.Serialports = new System.Windows.Forms.ComboBox();
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.path = new System.Windows.Forms.Label();
+            this.browse_btn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.filename = new System.Windows.Forms.TextBox();
+            this.openFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.Record = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Excel_writing = new System.ComponentModel.BackgroundWorker();
+            this.status = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datasets Builder";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.disconnect_btn);
+            this.groupBox1.Controls.Add(this.baudrates);
+            this.groupBox1.Controls.Add(this.Serialports);
+            this.groupBox1.Controls.Add(this.connect_btn);
+            this.groupBox1.Location = new System.Drawing.Point(8, 164);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 150);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial connection";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Select baud rate :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select COM port :";
+            // 
+            // disconnect_btn
+            // 
+            this.disconnect_btn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.disconnect_btn.Enabled = false;
+            this.disconnect_btn.Location = new System.Drawing.Point(190, 119);
+            this.disconnect_btn.Name = "disconnect_btn";
+            this.disconnect_btn.Size = new System.Drawing.Size(90, 25);
+            this.disconnect_btn.TabIndex = 3;
+            this.disconnect_btn.Text = "Disconnect";
+            this.disconnect_btn.UseVisualStyleBackColor = true;
+            this.disconnect_btn.Click += new System.EventHandler(this.disconnect_btn_Click);
+            // 
+            // baudrates
+            // 
+            this.baudrates.FormattingEnabled = true;
+            this.baudrates.Items.AddRange(new object[] {
+            "300",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "74880",
+            "115200",
+            "230400"});
+            this.baudrates.Location = new System.Drawing.Point(130, 82);
+            this.baudrates.Name = "baudrates";
+            this.baudrates.Size = new System.Drawing.Size(121, 24);
+            this.baudrates.TabIndex = 1;
+            // 
+            // Serialports
+            // 
+            this.Serialports.FormattingEnabled = true;
+            this.Serialports.Location = new System.Drawing.Point(130, 34);
+            this.Serialports.Name = "Serialports";
+            this.Serialports.Size = new System.Drawing.Size(121, 24);
+            this.Serialports.TabIndex = 0;
+            // 
+            // connect_btn
+            // 
+            this.connect_btn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.connect_btn.Location = new System.Drawing.Point(7, 119);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(90, 25);
+            this.connect_btn.TabIndex = 2;
+            this.connect_btn.Text = "Connect";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.path);
+            this.groupBox2.Controls.Add(this.browse_btn);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.filename);
+            this.groupBox2.Location = new System.Drawing.Point(323, 164);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(286, 150);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Excel connection";
+            // 
+            // path
+            // 
+            this.path.AutoSize = true;
+            this.path.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.path.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.path.Location = new System.Drawing.Point(120, 85);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(0, 13);
+            this.path.TabIndex = 10;
+            // 
+            // browse_btn
+            // 
+            this.browse_btn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.browse_btn.Location = new System.Drawing.Point(228, 79);
+            this.browse_btn.Name = "browse_btn";
+            this.browse_btn.Size = new System.Drawing.Size(30, 23);
+            this.browse_btn.TabIndex = 9;
+            this.browse_btn.Text = "...";
+            this.browse_btn.UseVisualStyleBackColor = true;
+            this.browse_btn.Click += new System.EventHandler(this.browse_btn_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "File path :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(225, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = ".xlsx";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "File name :";
+            // 
+            // filename
+            // 
+            this.filename.Location = new System.Drawing.Point(122, 36);
+            this.filename.Name = "filename";
+            this.filename.Size = new System.Drawing.Size(100, 22);
+            this.filename.TabIndex = 3;
+            // 
+            // Record
+            // 
+            this.Record.Enabled = false;
+            this.Record.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Record.Location = new System.Drawing.Point(8, 345);
+            this.Record.Name = "Record";
+            this.Record.Size = new System.Drawing.Size(80, 35);
+            this.Record.TabIndex = 3;
+            this.Record.Text = "Record";
+            this.Record.UseVisualStyleBackColor = true;
+            this.Record.Click += new System.EventHandler(this.Record_Click_2);
+            // 
+            // Stop
+            // 
+            this.Stop.Enabled = false;
+            this.Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop.Location = new System.Drawing.Point(529, 345);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(80, 35);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Excel_writing
+            // 
+            this.Excel_writing.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Excel_writing_DoWork);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Location = new System.Drawing.Point(241, 362);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 20);
+            this.status.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(123, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.status);
+            this.panel1.Controls.Add(this.Record);
+            this.panel1.Controls.Add(this.Stop);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(619, 380);
+            this.panel1.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(435, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 56);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(156, 387);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(318, 15);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Credit : Baptiste ZLOCH (MakeIT owner) March 2021 v1.0";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // datasets_builder_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(622, 411);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Name = "datasets_builder_form";
+            this.Text = "MakeIT : Datasets Builder";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button disconnect_btn;
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.ComboBox baudrates;
+        private System.Windows.Forms.ComboBox Serialports;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox filename;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button browse_btn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.FolderBrowserDialog openFolderDialog;
+        private System.Windows.Forms.Label path;
+        private System.Windows.Forms.Button Record;
+        private System.Windows.Forms.Button Stop;
+        private System.ComponentModel.BackgroundWorker Excel_writing;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Datasets_builder/datasets_builder_form.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Arduino_to_Csharp_to_excel
+{
+    public partial class datasets_builder_form : Form
+    {
+        String port_name = "";
+        int[] bauds_rates = new int[] { 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400 };
+        int baud_rate;
+        SerialPort _serialPort;
+
+        Excel.Application objApp;
+        Excel.Workbook objBook;
+        Excel.Worksheet objSheet;
+
+        String file_name = "";
+        String file_path = "";
+        public datasets_builder_form()
+        {
+            InitializeComponent();
+            Serialports.DataSource = SerialPort.GetPortNames();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (baud_rate.ToString() != "")
+            {
+                if ((string)Serialports.SelectedItem != "")
+                {
+                    baud_rate = bauds_rates[baudrates.SelectedIndex];
+                    Console.WriteLine("Baud rate : " + baud_rate);
+                    port_name = (string)Serialports.SelectedItem;
+                    Console.WriteLine("COM port : " + port_name);
+                    _serialPort = new SerialPort(port_name, baud_rate);
+                    _serialPort.Open();
+                    if (_serialPort.IsOpen)
+                    {
+                        Console.WriteLine("Connected !");
+                        disconnect_btn.Enabled = true;
+                        connect_btn.Enabled = false;
+                        Record.Enabled = true;
+                        status.Text = "Status : Connected";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("COM port not selected !", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Baud rate not selected !", "Error");
+            }
+            Console.WriteLine("Well connected !");
+        }
+
+        private void disconnect_btn_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null && _serialPort.IsOpen)
+            {
+                _serialPort.Close();
+                baudrates.Text = "";
+                connect_btn.Enabled = true;
+                disconnect_btn.Enabled = false;
+                Console.WriteLine("Disconnected !");
+                status.Text = "Status : Connected";
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Stop recording !");
+            status.Text = "Status : Stopped";
+            Excel_writing.WorkerSupportsCancellation = true;
+            Excel_writing.CancelAsync();
+            object misValue = System.Reflection.Missing.Value;
+            if (file_path != "")
+            {
+                if (filename.Text != "")
+                {
+                    file_name = filename.Text;
+                    Console.WriteLine("Writing file at : " + file_path + "\\" + file_name + ".xlsx");
+                    objBook.SaveAs(file_path + "\\" + file_name, Excel.XlFileFormat.xlOpenXMLWorkbook, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                    objBook.Close(true, misValue, misValue);
+                    objApp.Quit();
+                    Stop.Enabled = false;
+                    Record.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("File name empty !", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("File path not selected !", "Error");
+            }
+
+        }
+
+        private void Excel_writing_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("Start recording !");
+                //status.Text = "Status : Recording...";
+                _serialPort.Write("1");//Send one that mean the software is waiting for the headers
+                int j = 1;
+                String line = _serialPort.ReadLine();
+                String[] column_array;
+                //reading the header of the data
+                if (line.Contains(";"))
+                {
+                    Console.WriteLine("Current line : " + line);
+                    column_array = line.Split(';');
+                    for (int i = 0; i < column_array.Length; i++)
+                    {
+                        objSheet.Cells[1, i + 1] = column_array[i];
+                    }
+                }
+                Console.WriteLine("Before while");
+                //reading serial's content until the user click on stop that cancels the background worker
+                while (!Excel_writing.CancellationPending)
+                {
+                    _serialPort.Write("2"); //send 2 that mean the software is waiting for the data.
+                    line = _serialPort.ReadLine();
+                    if (line.Contains(";"))
+                    {
+                        Console.WriteLine("Current line : " + line);
+                        column_array = line.Split(';');
+                        for (int i = 0; i < column_array.Length; i++)
+                        {
+                            objSheet.Cells[j + 1, i + 1] = column_array[i];
+                        }
+                        Console.WriteLine("Capture in progress...");
+                        j++;
+                    }
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+
+            }
+        }
+
+        private void Record_Click_2(object sender, EventArgs e)
+        {
+            if (_serialPort != null && _serialPort.IsOpen)
+            {
+                if (filename.Text != null)
+                {
+                    file_name = filename.Text;
+                    Console.WriteLine("Opening excel app !");
+                    objApp = new Excel.Application();
+                    if (objApp == null)
+                    {
+                        MessageBox.Show("Excel not installed on your computer !", "Error");
+                        return;
+                    }
+                    object misValue = System.Reflection.Missing.Value;
+                    objBook = objApp.Workbooks.Add(misValue);
+                    objSheet = (Excel.Worksheet)objBook.Worksheets.get_Item(1);
+                    Stop.Enabled = true;
+                    Record.Enabled = false;
+                    status.Text = "Status : Recording...";
+                    Excel_writing.RunWorkerAsync();
+                }
+                else
+                {
+                    MessageBox.Show("Put a filename !", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Open a serial port !", "Error");
+            }
+        }
+
+        private void browse_btn_Click_1(object sender, EventArgs e)
+        {
+            if (openFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                file_path = openFolderDialog.SelectedPath;
+                Console.WriteLine("File path : " + file_path);//14 char
+                if (file_path.Length > 20)
+                {
+                    path.Text = file_path.Substring(0, 7) + "..." + file_path.Substring(file_path.Length - 10, 10);
+                    Console.WriteLine("Short file path : " + path.Text);//20 char
+                    status.Text = "Status : folder loaded";
+                }
+                else
+                {
+                    path.Text = file_path;
+                }
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/channel/UC_z_SHFipTRltrwM6T_KsFg");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.arduino.cc/");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.microsoft.com/fr-fr/microsoft-365/excel");
+        }
+    }
+}
